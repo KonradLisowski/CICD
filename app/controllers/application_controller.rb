9df50ff80b353542bf92e4c8d43f34c56a87709f
KonradLisowski <vkonradlisowskiv@outlook.com>
2024-12-03 10:06:08 +0000

    add posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
+# This is the top level post controller comment
 # frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
